7aa743e33f002893b46c9d0a8d2c63873be5549b
Jyun <njyun666666@gmail.com>
2022-08-05 10:51:50 +0800

    update url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://njyun666666.github.io/note-docusaurus',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
