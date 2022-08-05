c96d24d3e7b7d2f2f3b50430e2ef8ec942358e3e
Jyun <njyun666666@gmail.com>
2022-08-05 11:34:48 +0800

    update url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://njyun666666.github.io',
-  baseUrl: '/note-docusaurus',
+  baseUrl: '/note-docusaurus/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
